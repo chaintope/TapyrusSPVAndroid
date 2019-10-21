e07a5f7d4fcf89c4605c80af331f6f4ac00eda6d
Kohei Taniguchi <kohei@chaintope.com>
2019-10-21 14:15:06 +0900

    Initial commit.

---
+++ app/src/main/java/com/chaintope/tapyrus/spv/MainActivity.java
@@ -0,0 +1,23 @@
+package com.chaintope.tapyrus.spv;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    static {
+        System.loadLibrary("tapyrus_spv");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RustGreetings g = new RustGreetings();
+        String r = g.sayHello("Android world");
+        ((TextView)findViewById(R.id.greetingField)).setText(r);
+    }
+}
---
+++ app/src/main/java/com/chaintope/tapyrus/spv/RustGreetings.java
@@ -0,0 +1,10 @@
+package com.chaintope.tapyrus.spv;
+
+public class RustGreetings {
+
+    private static native String greeting(final String pattern);
+
+    public String sayHello(String to) {
+        return greeting(to);
+    }
+}
